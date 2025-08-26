cb1f7504a9993fcb8a62c94df43307ddace908e6
Siddharth Mathur <siddharth@getfastah.com>
2025-08-26 20:20:06 +0530

    Remove unused files

--- configuration_extras.go
+++
@@ -1,7 +0,0 @@
-package libattio
-
-var (
-	// ContextAPIKey can be used as a key name for context.WithValue()'s second parameter,
-	// allowing you to pass an API key to functions via the context.
-	ContextAPIKey = contextKey("apiKey")
-)
